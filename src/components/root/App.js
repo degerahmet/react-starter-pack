6733ed45d2f0c74d78259aa6f638d79ad55ff6a6
Deger <adeger34@gmail.com>
2020-12-13 17:00:08 +0300

    essential libs added.

--- src/components/root/App.js
+++ src/components/root/App.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react'
-
+//import { connect } from 'react-redux';
+//import {alertify} from "alertifyjs";
 
 class App extends Component {
+
+  // componentDidCatch(){   //render ardından ilk çağırılan statik js methodudur.
+  //   this.getAPIRequest();
+  // }
+
+  // getAPIRequest = () => {
+  //   fetch("URL")
+  //   .then(response=>response.json())
+  //   .then(data=>this.setState({data:data}));; //gelen datayı elimizdeki data ile değiştirdik
+  // }
+
   render() { 
     return (
     <div>
@@ -12,3 +24,12 @@ class App extends Component {
 }
 
 export default App;
+
+
+// function mapStateToProps(state){
+//   return {
+//       data:state.changeDataReducer
+//   }
+// }
+
+// export default connect(mapStateToProps)(App)
--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/root/App';
+import "bootstrap/dist/css/bootstrap.min.css";
+    
+import {BrowserRouter,Route,Switch} from "react-router-dom";
 
-ReactDOM.render(<App />,document.getElementById('root'));
+//import {Provider} from "react-redux";
+//import configureStore from './redux/reducers/configureStore';
+//const store = configureStore();
+
+ReactDOM.render(
+// <Provider store={store}>
+<App />
+// </Provider> 
+,document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
